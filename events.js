b60acc0580eb58866e9f3333011691cd7840c61c
Hans-Wilhelm Warlo <hw@warlo.no>
2017-03-02 10:37:52 +0100

    Update event descriptions

--- events.js
+++ events.js
@@ -9,78 +9,81 @@ export default [{
   abakus: 'https://abakus.no/event/1990-apningsfest/',
   facebook: 'https://www.facebook.com/events/264402880662411/'
 }, {
-  title: 'Glassjakt (ila. dagen)',
+  title: 'Glassjakt (i løpet av dagen)',
   startsAt: parse('March 13, 2017 10:00'),
   endsAt: parse('March 13, 2017 15:00'),
-  location: 'Downtown',
-  description: 'Jubileumsuken er i gang, og det håper vi at du også er! Vi kickstarter uken med en spennende lek Vil du påstå at du har et øye for detaljer? Rundt omkring i kriker og kroker på Gløshaugen gjemmer det seg noe helt spesielt.',
-  abakus: 'https://abakus.no/event/123',
-  facebook: '/123'
+  location: 'Gløshaugen',
+  description: 'Jubileumsuken er i gang, og det håper vi at du også er! Vi kickstarter uken med en spennende lek Vil du påstå at du har et øye for detaljer? Rundt omkring i kriker og kroker på Gløshaugen gjemmer det seg noe helt spesielt.'
 }, {
-  title: 'Slipp av lang proggekonk',
+  title: 'Slipp av lang programmeringskonkurranse',
   startsAt: parse('March 13, 2017 12:00'),
-  abakus: 'https://abakus.no/event/123',
-  facebook: '/123'
+  description: 'Mer info kommer'
 }, {
   title: 'Kurs med Capra Consulting',
   startsAt: parse('March 13, 2017 16:00'),
   endsAt: parse('March 13, 2017 23:59'),
+  location: 'IT-vest 454',
   description: 'Kunne du tenke deg å utvikle noe greier for mobile plattformer men også tilegne deg kunnskap som er så og si direkte anvendbart for generell frontendutvikling i JavaScript? Da kan dette kurset i React Native være noe for deg.',
-  abakus: 'https://abakus.no/event/123',
+  abakus: 'https://abakus.no/event/1997-kurs-i-react-native-med-capra-consulting/',
   facebook: 'https://www.facebook.com/events/1940302012857992/'
 }, {
   title: 'π-dagen',
   startsAt: parse('March 14, 2017 10:00'),
   endsAt: parse('March 14, 2017 15:00'),
   location: 'Kontoret',
-  description: 'Gjør deg klar for nok en gøyal dag på kontoret! I dag feirer vi Π-dagen. Den matematiske konstanten som er definert som forholdet mellom omkretsen og diameteren til en sirkel har fått en stor plass i hjerte til enhver gløsing.',
-  abakus: 'https://abakus.no/event/123',
-  facebook: '/123'
+  description: 'Gjør deg klar for nok en gøyal dag på kontoret! I dag feirer vi Π-dagen. Den matematiske konstanten som er definert som forholdet mellom omkretsen og diameteren til en sirkel har fått en stor plass i hjerte til enhver gløsing.'
 }, {
   title: 'Pub-til-pub med Bekk',
   startsAt: parse('March 14, 2017 17:00'),
+  endsAt: parse('March 14, 2017 23:59'),
+  location: 'Midtbyen',
   description: 'BEKK arrangerer Pub-til-Pub Rebus! Oppmøte foran Hovedbygget hvor vi tar felles buss ned til midtbyen for bespisning. Deretter danner vi lag og starter Pub-til-Pub runden, hvor det vil være ulike poster på hvert sted. Velkommen!',
   abakus: 'https://abakus.no/event/1994-bekk/',
   facebook: 'https://www.facebook.com/events/423137358027660/'
 }, {
   title: 'Barnas dag',
   startsAt: parse('March 15, 2017 10:00'),
+  location: 'Glassgården',
   description: 'Abakus blir 40 år og midtlivskrisen slår til. Det er ikke noe bedre tidspunkt å slå til med aktiviteter som får en til å føle seg som et barn igjen!',
   abakus: 'https://abakus.no/event/1995-barnas-dag/',
   facebook: 'https://www.facebook.com/events/106647736530334/'
 }, {
   title: 'LAN',
   startsAt: parse('March 15, 2017 16:00'),
+  location: 'P15',
   description: 'Onsdag 15. mars er det klart for LAN på P15. Det blir god stemning, med mange kule konkurranser, fete premier og selvfølgelig pizza!',
   abakus: 'https://abakus.no/event/1992-lan/',
   facebook: 'https://www.facebook.com/events/261345810987335/'
 }, {
-  title: 'Kaketorsdag (stor kake)',
+  title: 'Kaketorsdag',
   startsAt: parse('March 16, 2017 10:00'),
-  description: 'Ordet jubileum eksisterer ikke uten ordet kake, så i dag feirer vi 40 år med et stykke kunst av et bakstverk. Dette er ukas beste mulighet for å sette sommerkroppen på vent og glede ditt usynlige deg med endorfiner bare et sukkerrush fra en annen verden kan gi deg.',
-  abakus: 'https://abakus.no/event/123',
-  facebook: '/123'
+  location: 'Kontoret',
+  description: 'Ordet jubileum eksisterer ikke uten ordet kake, så i dag feirer vi 40 år med et stykke kunst av et bakstverk. Dette er ukas beste mulighet for å sette sommerkroppen på vent og glede ditt usynlige deg med endorfiner bare et sukkerrush fra en annen verden kan gi deg.'
 }, {
   title: 'Revy',
   startsAt: parse('March 16, 2017 19:30'),
+  location: 'Byscenen',
   description: '16. og 17. mars spiller Abakus sin aller første revy på byscenen. Billetter kjøpes på byscenen.no',
   abakus: 'https://abakus.no/event/1979-abakusrevyen-2017/',
   facebook: 'https://www.facebook.com/events/432063403794600/'
 }, {
   title: 'Labamba premierefest',
   startsAt: parse('March 16, 2017 21:30'),
+  location: 'Trondhjem Seilforening',
   description: 'Har revyen gitt deg sommerfugler i magen og vekket danskefoten? Vi vet hvor du kan slippe deg løs! Etter premieren til Abakusrevyen, torsdag 16 mars, står LaBamba klare til å serve deg på Trondhjem Seilforening.  Og husk, cash is king!',
   abakus: 'https://abakus.no/event/1996-premierefest/',
   facebook: 'https://www.facebook.com/events/1869604529975848/'
 }, {
   title: 'Revy',
   startsAt: parse('March 17, 2017 19:30'),
+  location: 'Byscenen',
   description: '16. og 17. mars spiller Abakus sin aller første revy på byscenen. Billetter kjøpes på byscenen.no',
   abakus: 'https://abakus.no/event/1979-abakusrevyen-2017/',
   facebook: 'https://www.facebook.com/events/432063403794600/'
 }, {
   title: 'GALLA',
   startsAt: parse('March 18, 2017 18:00'),
+  location: 'Scandic Lerkendal',
   description: 'Lørdag 18. mars klokken 18:00 er det duket for studentergalla på Scandic Lerkendal. Det vil bli servert 2-retters middag samt to drikkeenheter. Det blir storartet underholdning fra start til slutt, og Ababand skal selvfølgelig spille. Rundt midnatt forflytter vi oss til nach der festlighetene fortsetter utover natten.',
   abakus: 'https://abakus.no/event/1958-jubileumsgalla/',
   facebook: 'https://www.facebook.com/events/1327162840679440/'
